7ce87f541e1089ed913f7c6d5029bb9cd98436a5
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-09-28 18:12:05 +0100

    added basic layout of html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
