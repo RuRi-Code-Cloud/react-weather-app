0a4825fbe39b873c28bebd9f49093a067dd08e2a
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-09-28 16:54:55 +0100

    added footer with links of github and linkedin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <h1>Weather</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/rita-rudzite-6440a0271/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rita Rudzite{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/RuRi-Code-Cloud/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
