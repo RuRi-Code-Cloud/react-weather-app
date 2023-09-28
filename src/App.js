2fdf8379bfcb9bb0ea1875e185f077162d5687ad
RuRi-Code-Cloud <rudziter367@gmail.com>
2023-09-28 17:37:02 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/rita-rudzite-6440a0271/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rita Rudzite{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/RuRi-Code-Cloud/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/rita-rudzite-6440a0271/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rita Rudzite{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/RuRi-Code-Cloud/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
